c6e5971b8f0a472f7329e982faee70a4d7865a93
Gitnapp <41632082+Gitnapp@users.noreply.github.com>
2024-03-30 22:51:23 +0800

    Quartz sync: Mar 30, 2024, 10:51 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -12,7 +12,7 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: { provider: 'google', tagId: 'G-YD9QB88Q1F' },
-    locale: "zh-CN",
+    locale: "en-US",
     baseUrl: "12701.top",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",    // created, modified, or published
